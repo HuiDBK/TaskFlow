9d4b042e82036564a7b514426ff7222bc6417f02
zhengxiaoquan <zhengxiaoquan@fuzhi.ai>
2023-08-29 11:17:35 +0800

    项目搭建以及部分校验模型

---
+++ src/logics/user/login.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(description="login")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
